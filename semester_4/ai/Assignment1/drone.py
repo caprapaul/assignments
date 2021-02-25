c090544bb012b46e63ac691129eecacb3ad85558
Paul Capra <c.paulica@gmail.com>
2021-02-25 21:21:39 +0200

    Added AI mapper

---
+++ semester_4/ai/Assignment1/drone.py
@@ -0,0 +1,75 @@
+import pygame
+from constants import *
+from pygame.locals import *
+
+
+class Drone():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.visited = set()
+        self.visited.add((x, y))
+        self.back = [(x, y)]
+
+    def is_finished(self):
+        return (self.x, self.y) == (None, None)
+
+    def move(self, detected_map):
+        pressed_keys = pygame.key.get_pressed()
+        if self.x > 0:
+            if pressed_keys[K_UP] and detected_map.surface[self.x-1][self.y] == 0:
+                self.x = self.x - 1
+        if self.x < 19:
+            if pressed_keys[K_DOWN] and detected_map.surface[self.x+1][self.y] == 0:
+                self.x = self.x + 1
+
+        if self.y > 0:
+            if pressed_keys[K_LEFT] and detected_map.surface[self.x][self.y-1] == 0:
+                self.y = self.y - 1
+        if self.y < 19:
+            if pressed_keys[K_RIGHT] and detected_map.surface[self.x][self.y+1] == 0:
+                self.y = self.y + 1
+
+    def get_neighbors(self, detected_map):
+        neighbors = []
+
+        if self.x > 0:
+            if detected_map.surface[self.x - 1][self.y] == 0:
+                neighbors.append((self.x - 1, self.y))
+
+        if self.y > 0:
+            if detected_map.surface[self.x][self.y - 1] == 0:
+                neighbors.append((self.x, self.y - 1))
+
+        if self.x < 19:
+            if detected_map.surface[self.x + 1][self.y] == 0:
+                neighbors.append((self.x + 1, self.y))
+
+        if self.y < 19:
+            if detected_map.surface[self.x][self.y + 1] == 0:
+                neighbors.append((self.x, self.y + 1))
+
+        return neighbors
+
+    def move_DSF(self, detected_map):
+        # TO DO!
+        # rewrite this function in such a way that you perform an automatic
+        # mapping with DFS
+
+        unvisited_neighbors = list(
+            filter(lambda x: x not in self.visited,
+                   self.get_neighbors(detected_map))
+        )
+
+        if not unvisited_neighbors:
+            if not self.back:
+                (self.x, self.y) = (None, None)
+                return
+
+            (self.x, self.y) = self.back.pop()
+            return
+
+        self.back.append((self.x, self.y))
+        new_pos = unvisited_neighbors.pop()
+        (self.x, self.y) = new_pos
+        self.visited.add(new_pos)
---
+++ semester_4/ai/Assignment1/vsimple.py
@@ -0,0 +1,106 @@
+
+
+# import the pygame module, so you can use it
+import pygame
+from pygame.locals import *
+from random import randint
+
+from environment import Environment
+from dmap import DMap
+from drone import Drone
+
+from constants import *
+
+from tkinter import *
+from tkinter import messagebox
+
+
+def main():
+    # we create the environment
+    e = Environment()
+    e.load_environment("test2.map")
+    # print(str(e))
+
+    # we create the map
+    m = DMap()
+
+    # initialize the pygame module
+    pygame.init()
+    # load and set the logo
+    logo = pygame.image.load("logo32x32.png")
+    pygame.display.set_icon(logo)
+    pygame.display.set_caption("drone exploration")
+
+    # we position the drone somewhere in the area
+    x = randint(0, 19)
+    y = randint(0, 19)
+
+    # cream drona
+    d = Drone(x, y)
+
+    # create a surface on screen that has the size of 800 x 480
+    width = 800
+    height = 400
+    screen = pygame.display.set_mode((width, height))
+    screen.fill(WHITE)
+    screen.blit(e.image(), (0, 0))
+
+    font = pygame.font.SysFont('Arial', 24)
+    img = font.render('Finished', True, BLACK, WHITE)
+    rect = img.get_rect()
+    rect.center = (width // 2, height)
+
+    # define a variable to control the main loop
+    running = True
+    auto = True
+    delay = 500
+
+    message_shown = False
+
+    # main loop
+    while running:
+        m.mark_detected_walls(e, d.x, d.y)
+
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+
+            if event.type == KEYDOWN:
+                # change auto movement speed
+                pressed_keys = pygame.key.get_pressed()
+
+                if pressed_keys[K_UP]:
+                    delay //= 2
+
+                if pressed_keys[K_DOWN]:
+                    delay *= 2
+
+                delay = max(10, delay)
+
+                if (not auto) and not d.is_finished():
+                    d.move_DSF(m)
+
+        if auto and not d.is_finished():
+            d.move_DSF(m)
+            pygame.time.delay(delay)
+
+        screen.blit(m.image(d.x, d.y), (400, 0))
+
+        if d.is_finished() and not message_shown:
+            Tk().wm_withdraw()
+            messagebox.showinfo('Job\'s done!', 'Finished map.')
+            message_shown = True
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+# run the main function only if this module is executed as the main script
+# (if you import this as a module then nothing is executed)
+if __name__ == "__main__":
+    # call the main function
+    main()

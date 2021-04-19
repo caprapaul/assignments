d51404bfaec4c639a152293ca293203e9cf6e41b
Paul Capra <c.paulica@gmail.com>
2021-04-19 16:16:09 +0300

    Added cluster stats

--- semester_4/ai/Assignment6/main.py
+++ semester_4/ai/Assignment6/main.py
@@ -4,6 +4,17 @@ import random
 EPSILON = 0.001
 
 
+class Stats:
+    def __init__(self, accuracy, precision, recall, f_score):
+        self.accuracy = accuracy
+        self.precision = precision
+        self.recall = recall
+        self.f_score = f_score
+
+    def __str__(self):
+        return f'accuracy: {self.accuracy}\n precision: {self.precision}\n recall: {self.recall}\n f_score: {self.f_score}\n'
+
+
 class Point:
     def __init__(self, correct_label, x, y):
         self.correct_label = correct_label
@@ -25,8 +36,8 @@ class Point:
 
 
 class Cluster:
-    def __init__(self, label):
-        self.label = label
+    def __init__(self):
+        self.label = 'X'
         self.points = []
         self.mean_x = 0
         self.mean_y = 0
@@ -42,6 +53,7 @@ class Cluster:
 
     def remove_point(self, point):
         self.points.remove(point)
+        self.update_mean()
 
     def update_mean(self):
         """Updates the mean and returns True if the mean is changed"""
@@ -53,6 +65,31 @@ class Cluster:
         self.mean_y = sum_y / len(self.points)
 
         return not (approximately_equal(self.mean_x, old_mean_x, EPSILON) and approximately_equal(self.mean_y, old_mean_y, EPSILON))
+
+    def update_label(self):
+        labels_list = [point.correct_label for point in self.points]
+        dominant_label = max(set(labels_list), key=labels_list.count)
+        self.label = dominant_label
+
+    def get_stats(self, points):
+        tp = [point for point in self.points if point.correct_label == self.label]
+        fp = [point for point in self.points if point.correct_label != self.label]
+        tn = [point for point in points if point.cluster.label !=
+              self.label and point not in self.points]
+        fn = [point for point in points if point.cluster.label ==
+              self.label and point not in self.points]
+
+        tp = len(tp)
+        fp = len(fp)
+        tn = len(tn)
+        fn = len(fn)
+
+        accuracy = (tp + tn) / (tp + tn + fp + fn)
+        precision = tp / (tp + fp)
+        recall = tp / (tp + fn)
+        f_score = 2 / ((1 / recall) / (1 / precision))
+
+        return Stats(accuracy, precision, recall, f_score)
 
 
 def distance(x1, y1, x2, y2):
@@ -89,12 +126,42 @@ def save_points(points, file_path):
         writer.write(output)
 
 
+def load_clusters(file_path):
+    lines = []
+    clusters = []
+    with open(file_path) as reader:
+        lines = reader.readlines()
+
+    lines = lines[1:]
+    for line in lines:
+        label, x, y = line.split(',')
+        x = float(x)
+        y = float(y)
+        cluster = Cluster()
+        cluster.label = label
+        cluster.mean_x = x
+        cluster.mean_y = y
+        clusters.append(cluster)
+
+    return clusters
+
+
+def save_clusters(clusters, file_path):
+    output = 'label,mean_x,_mean_y\n'
+
+    for cluster in clusters:
+        output += f'{cluster.label},{cluster.mean_x},{cluster.mean_y}\n'
+
+    with open(file_path, 'w') as writer:
+        writer.write(output)
+
+
 def classify(points):
     clusters = [
-        Cluster('A'),
-        Cluster('B'),
-        Cluster('C'),
-        Cluster('D')
+        Cluster(),
+        Cluster(),
+        Cluster(),
+        Cluster()
     ]
 
     random_points = random.sample(points, len(clusters))
@@ -118,14 +185,21 @@ def classify(points):
     return clusters
 
 
-def fix_labels(clusters):
+def update_labels(clusters):
     for cluster in clusters:
-        labels_list = [point.correct_label for point in cluster.points]
-        dominant_label = max(set(labels_list), key=labels_list.count)
-        cluster.label = dominant_label
+        cluster.update_label()
+
+
+def print_stats(points, clusters):
+    clusters =
+    for cluster in clusters:
+        stats = cluster.get_stats(points)
+        print(f'{cluster.label}:\n {stats}')
 
 
 points = load_points('dataset.csv')
 clusters = classify(points)
-fix_labels(clusters)
-save_points(points, 'output.csv')
+update_labels(clusters)
+print_stats(points, clusters)
+save_clusters(clusters, 'output_clusters.csv')
+save_points(points, 'output_points.csv')
